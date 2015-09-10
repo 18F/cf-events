abe15e2533adfc303dc871194c894e3b1d40594c
ramirezg <gabriel.ramirez@gsa.gov>
2015-09-10 09:12:45 -0400

    adding vcap parser for GO

--- helpers/mongo.go
+++ helpers/mongo.go
@@ -1,13 +1,28 @@
 package helpers
 
 import (
+	"fmt"
+	"log"
+
+	cfenv "github.com/cloudfoundry-community/go-cfenv"
 	"gopkg.in/mgo.v2"
 )
 
 func GetMongoSession() *mgo.Session {
-	// Connect to our local mongo
-	s, err := mgo.Dial("mongodb://localhost")
-
+	var mongoURI string
+	// Get the VCAP env
+	appEnv, _ := cfenv.Current()
+	if appEnv != nil {
+		// Get services
+		services, _ := appEnv.Services.WithTag("mongodb")
+		// Get mongo service info
+		mongoURI = fmt.Sprint(services[0].Credentials["uri"])
+	} else {
+		log.Println("Defaulting to local mongo database")
+		mongoURI = "mongodb://localhost"
+	}
+	// Setup session
+	s, err := mgo.Dial(mongoURI)
 	// Check if connection error, is mongo running?
 	if err != nil {
 		panic(err)
